711e934b901c6020287b6acd58e1fba6b7927228
mayuri-16 <mayuriubale2000@gmail.com>
2024-02-13 20:06:43 +0530

    latest commit

--- graphQLQuery/graphQLQuery.js
+++ graphQLQuery/graphQLQuery.js
@@ -1,15 +1,35 @@
 import { LightningElement,wire,track } from 'lwc';
 import { gql, graphql } from "lightning/uiGraphQLApi";
 
+
+const columns = [
+  {label : "Name", fieldName : "Name" , type:'text'},
+  {label : "Phone", fieldName : "Phone" , type:'phone'},
+  {label : "Email", fieldName : "Email" , type:'email'},
+  {label : "Account Name", fieldName : "AccountName" , type:'Text'},
+  {label : "Created Date", fieldName : "CreatedDate" , type:'date'},
+  
+]
+
 export default class GraphQLQuery extends LightningElement {
 
       @track records;
       searchValue='';
+      dataList= [];
+      columnsList =columns; 
+      after =null;
+      pageInfo;
+      pageNumber=1;
+      totalCount=0;
+      pageSize=5;
+      isLoading=false;
+
 
       get variables(){
         return {
             likeParams: '%'+this.searchValue+'%',
-            limit:5
+            limit:this.pageSize,
+            after:this.after
         }
       }
 
@@ -19,11 +39,16 @@ export default class GraphQLQuery extends LightningElement {
         
       }
 
+      connectedCallback(){
+        this.isLoading=true;
+      }
+
     @wire(graphql, {
        query: gql`
        query ContactInfo (
         $likeParams: String,
-        $limit: Int
+        $limit: Int,
+        $after: String
        )
        {
         uiapi {
@@ -33,7 +58,8 @@ export default class GraphQLQuery extends LightningElement {
                 orderBy: {Name: {order :ASC}},
                 where: {
                     Name: {like: $likeParams}
-                }
+                },
+                after: $after
             ) {
               edges {
                 node {
@@ -41,7 +67,15 @@ export default class GraphQLQuery extends LightningElement {
                   Name {
                     value
                   }
+                  Phone{
+                    value
+                  }
+                  CreatedDate{
+                    value,
+                    displayValue
+                  }
                   Account{
+                    Id
                     Name{
                       value
                     }
@@ -51,6 +85,13 @@ export default class GraphQLQuery extends LightningElement {
                   }
                   
                 }
+              }
+              totalCount
+              pageInfo{
+                hasNextPage
+                hasPreviousPage
+                startCursor
+                endCursor
               }
             }
           }
@@ -62,11 +103,68 @@ export default class GraphQLQuery extends LightningElement {
     wiredGraphQLResult({data,error}){
         if(data){
            console.log('data ->>>'+JSON.stringify(data));
-           this.records=data.uiapi.query.Contact.edges;
+           //this.records=data.uiapi.query.Contact.edges;
+           this.pageInfo=data.uiapi.query?.Contact?.pageInfo;
+           this.totalCount=data.uiapi.query?.Contact?.totalCount;
+           this.dataList=data.uiapi.query?.Contact?.edges?.map((item)=>{
+            console.log("items "+item);
+            return {
+              Id: item.node.Id,
+              Name :item.node.Name.value,
+              Email :item.node.Email.value,
+              AccountName :item.node.Account?.Name.value,
+              CreatedDate :item.node.CreatedDate.value,
+              Phone :item.node.Phone.value,
+
+
+            }
+           });
+           this.isLoading=false;
         }
         else if(error){
             console.error('error'+error);
+            this.isLoading=false;
         }
-    }
+      }
+
+      get totalPage(){
+        return Math.ceil(this.totalCount /this.pageSize);
+      }
+
+      get disableNextButton(){
+        return !this.pageInfo?.hasNextPage;
+      }
+      get disableReset(){
+        if (this.pageNumber==1)
+        {
+          return true;
+        }
+        else{
+          return false;
+        }
+ 
+      }
+
+        handleNext(Event) {
+          Event.preventDefault();
+          this.isLoading=true;
+          if(this.pageInfo && this.pageInfo.hasNextPage)
+          {
+            this.after=this.pageInfo.endCursor;
+            this.pageNumber++;
+          }
+          else{
+            this.after=null;
+            this.pageNumber=1;
+          }
+
+        }
+
+        handleReset(Event){
+          Event.preventDefault();
+          this.isLoading=true;
+          this.after=null;
+          this.pageNumber=1;
+        }
 
 }
